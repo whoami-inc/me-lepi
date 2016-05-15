5b4b1fef5b55539c3b003a6a4876b77641b76428
IyoRTML <iyortml@gmail.com>
2016-05-16 05:42:33 +0700

    Update lib-me-lepi.js

--- lib-me-lepi.js
+++ lib-me-lepi.js
@@ -10,23 +10,3 @@ function prosses(){
 		window.location.assign("http://me-lepi.blogspot.com");
 	}
 }
-/* cari yuk */
-$(document).ready(function(){
-    $(".cari-yuk").click(function(){
-        $(".cari-yuk").css("background", "white");
-        $(".cari-yuk").css("color", "#2d2d2d");
-        $(".cari-yuk").css("opacity", "100");
-        $(".cari-yuk").css("filter", "100");
-    });
-});
-/* iklan script */
-$(document).ready(function(){
-    $("#btn1").click(function(){
-        $("#iklan-gratis").show();
-        $(".dekstop-home").hide();
-    });
-	$("#btn2").click(function(){
-        $(".dekstop-home").show();
-        $("#iklan-gratis").hide();
-    });
-});
