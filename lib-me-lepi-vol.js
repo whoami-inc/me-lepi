4a33e36e06c2e7d6941b983cbb7504fe68c6577c
IyoRTML <iyortml@gmail.com>
2016-05-17 01:14:02 +0700

    Create lib-me-lepi-vol.js

---
+++ lib-me-lepi-vol.js
@@ -0,0 +1,10 @@
+var x = document.getElementById(&quot;myMp3&quot;);
+function setNullVolume() { 
+    x.volume = 0.0;
+}
+function setMedVolume() { 
+    x.volume = 0.5;
+} 
+function setFullVolume() { 
+    x.volume = 1.0;
+} 
