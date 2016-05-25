44c8315b3257c9673fbc1253642316cc6536ffd7
IyoRTML <iyortml@gmail.com>
2016-05-26 05:08:02 +0700

    Create jQuery-lib-me-lepi-tema=5-25-16-3-coba.js

---
+++ jQuery-lib-me-lepi-tema=5-25-16-3-coba.js
@@ -0,0 +1,288 @@
+/*
+	Tema me-lepi
+	5/26/2016
+*/
+
+/* Tampilan Head Music Tampilan */
+function megOmUsicOrange() {
+    var x = document.getElementsByClassName("me-lepi-music-kehed");
+    x[0].style.background = "orange";
+    x[0].style.opacity = "0.6";
+    x[0].style.filter = "alpha(opacity=60)";
+    x[0].style.color = "#1d1d1d";
+}
+function megOmUsicCyan() {
+    var x = document.getElementsByClassName("me-lepi-music-kehed");
+    x[0].style.background = "cyan";
+    x[0].style.opacity = "0.6";
+    x[0].style.filter = "alpha(opacity=60)";
+    x[0].style.color = "#1d1d1d";
+}
+function megOmUsicIndigo() {
+    var x = document.getElementsByClassName("me-lepi-music-kehed");
+    x[0].style.background = "indigo";
+    x[0].style.opacity = "0.6";
+    x[0].style.filter = "alpha(opacity=60)";
+    x[0].style.color = "white";
+}
+function megOmUsicDimgray() {
+    var x = document.getElementsByClassName("me-lepi-music-kehed");
+    x[0].style.background = "dimgray";
+    x[0].style.opacity = "0.6";
+    x[0].style.filter = "alpha(opacity=60)";
+    x[0].style.color = "white";
+}
+function megOmUsicCrimson() {
+    var x = document.getElementsByClassName("me-lepi-music-kehed");
+    x[0].style.background = "crimson";
+    x[0].style.opacity = "0.6";
+    x[0].style.filter = "alpha(opacity=60)";
+    x[0].style.color = "white";
+}
+
+/* Tampilan Background Music Tampilan */
+function megOmUsicOrangeBG() {
+    var x = document.getElementsByClassName("me-lepi-music-tengah");
+    x[0].style.background = "orange";
+}
+function megOmUsicCyanBG() {
+    var x = document.getElementsByClassName("me-lepi-music-tengah");
+    x[0].style.background = "cyan";
+}
+function megOmUsicIndigoBG() {
+    var x = document.getElementsByClassName("me-lepi-music-tengah");
+    x[0].style.background = "indigo";
+}
+function megOmUsicDimgrayBG() {
+    var x = document.getElementsByClassName("me-lepi-music-tengah");
+    x[0].style.background = "dimgray";
+}
+function megOmUsicCrimsonBG() {
+    var x = document.getElementsByClassName("me-lepi-music-tengah");
+    x[0].style.background = "crimson";
+}
+
+/* Reset Music Tampilan */
+function megOmUsicResetAll() {
+    var x = document.getElementsByClassName("me-lepi-music-tengah");
+    x[0].style.background = "white";
+    x[0].style.color = "#1d1d1d";
+	var x = document.getElementsByClassName("me-lepi-music-kehed");
+    x[0].style.background = "orange";
+    x[0].style.opacity = "0.6";
+    x[0].style.filter = "alpha(opacity=60)";
+    x[0].style.color = "#1d1d1d";
+}
+
+/* Tampilan Head Chrome Tampilan */
+function meChromeOrange() {
+    var x = document.getElementsByClassName("me-lepi-chrome-kehed");
+    x[0].style.background = "orange";
+    x[0].style.opacity = "0.6";
+    x[0].style.filter = "alpha(opacity=60)";
+    x[0].style.color = "#1d1d1d";
+}
+function meChromeCyan() {
+    var x = document.getElementsByClassName("me-lepi-chrome-kehed");
+    x[0].style.background = "cyan";
+    x[0].style.opacity = "0.6";
+    x[0].style.filter = "alpha(opacity=60)";
+    x[0].style.color = "#1d1d1d";
+}
+function meChromeIndigo() {
+    var x = document.getElementsByClassName("me-lepi-chrome-kehed");
+    x[0].style.background = "indigo";
+    x[0].style.opacity = "0.6";
+    x[0].style.filter = "alpha(opacity=60)";
+    x[0].style.color = "white";
+}
+function meChromeDimgray() {
+    var x = document.getElementsByClassName("me-lepi-chrome-kehed");
+    x[0].style.background = "dimgray";
+    x[0].style.opacity = "0.6";
+    x[0].style.filter = "alpha(opacity=60)";
+    x[0].style.color = "white";
+}
+function meChromeCrimson() {
+    var x = document.getElementsByClassName("me-lepi-chrome-tengah");
+    x[0].style.background = "crimson";
+    x[0].style.opacity = "0.6";
+    x[0].style.filter = "alpha(opacity=60)";
+    x[0].style.color = "white";
+}
+
+/* Tampilan Background Chrome Tampilan */
+function meChromeOrangeBG() {
+    var x = document.getElementsByClassName("me-lepi-chrome-tengah");
+    x[0].style.background = "orange";
+}
+function meChromeCyanBG() {
+    var x = document.getElementsByClassName("me-lepi-chrome-tengah");
+    x[0].style.background = "cyan";
+}
+function meChromeIndigoBG() {
+    var x = document.getElementsByClassName("me-lepi-chrome-tengah");
+    x[0].style.background = "indigo";
+}
+function meChromeDimgrayBG() {
+    var x = document.getElementsByClassName("me-lepi-chrome-tengah");
+    x[0].style.background = "dimgray";
+}
+function meChromeCrimsonBG() {
+    var x = document.getElementsByClassName("me-lepi-chrome-tengah");
+    x[0].style.background = "crimson";
+}
+
+/*Reset Chrome Tampilan */
+function meChromeResetAll() {
+    var x = document.getElementsByClassName("me-lepi-chrome-tengah");
+    x[0].style.background = "white";
+    x[0].style.color = "#1d1d1d";
+	var x = document.getElementsByClassName("me-lepi-chrome-kehed");
+    x[0].style.background = "orange";
+    x[0].style.opacity = "0.6";
+    x[0].style.filter = "alpha(opacity=60)";
+    x[0].style.color = "#1d1d1d";
+}
+
+/* Tampilan Head Settings Tampilan*/
+function meSettOrange() {
+    var x = document.getElementsByClassName("me-lepi-sett-kehed");
+    x[0].style.background = "orange";
+    x[0].style.opacity = "0.6";
+    x[0].style.filter = "alpha(opacity=60)";
+    x[0].style.color = "#1d1d1d";
+}
+function meSettCyan() {
+    var x = document.getElementsByClassName("me-lepi-sett-kehed");
+    x[0].style.background = "cyan";
+    x[0].style.opacity = "0.6";
+    x[0].style.filter = "alpha(opacity=60)";
+    x[0].style.color = "#1d1d1d";
+}
+function meSettIndigo() {
+    var x = document.getElementsByClassName("me-lepi-sett-kehed");
+    x[0].style.background = "indigo ";
+    x[0].style.opacity = "0.6";
+    x[0].style.filter = "alpha(opacity=60)";
+    x[0].style.color = "white";
+}
+function meSettDimgray() {
+    var x = document.getElementsByClassName("me-lepi-sett-kehed");
+    x[0].style.background = "dimgray ";
+    x[0].style.opacity = "0.6";
+    x[0].style.filter = "alpha(opacity=60)";
+    x[0].style.color = "white";
+}
+function meSettCrimson() {
+    var x = document.getElementsByClassName("me-lepi-sett-kehed");
+    x[0].style.background = "crimson ";
+    x[0].style.opacity = "0.6";
+    x[0].style.filter = "alpha(opacity=60)";
+    x[0].style.color = "white";
+}
+
+/* Tampilan Background Settings Tampilan */
+function meSettOrangeBG() {
+    var x = document.getElementsByClassName("me-lepi-sett-tengah");
+    x[0].style.background = "orange";
+}
+function meSettCyanBG() {
+    var x = document.getElementsByClassName("me-lepi-sett-tengah");
+    x[0].style.background = "cyan";
+}
+function meSettIndigoBG() {
+    var x = document.getElementsByClassName("me-lepi-sett-tengah");
+    x[0].style.background = "indigo ";
+}
+function meSettDimgrayBG() {
+    var x = document.getElementsByClassName("me-lepi-sett-tengah");
+    x[0].style.background = "dimgray ";
+}
+function meSettCrimsonBG() {
+    var x = document.getElementsByClassName("me-lepi-sett-tengah");
+    x[0].style.background = "crimson ";
+}
+
+/*Reset Settings Tampilan */
+function meSettResetAll() {
+    var x = document.getElementsByClassName("me-lepi-sett-tengah");
+    x[0].style.background = "white ";
+    x[0].style.color = "#1d1d1d";
+	var x = document.getElementsByClassName("me-lepi-sett-kehed");
+    x[0].style.background = "orange";
+    x[0].style.opacity = "0.6";
+    x[0].style.filter = "alpha(opacity=60)";
+    x[0].style.color = "#1d1d1d";
+}
+
+/* Tampilan Head Apps Tampilan */
+function meOrange() {
+    var x = document.getElementsByClassName("me-lepi-kehed");
+    x[0].style.background = "orange";
+    x[0].style.opacity = "0.6";
+    x[0].style.filter = "alpha(opacity=60)";
+    x[0].style.color = "#1d1d1d";
+}
+function meCyan() {
+    var x = document.getElementsByClassName("me-lepi-kehed");
+    x[0].style.background = "cyan";
+    x[0].style.opacity = "0.6";
+    x[0].style.filter = "alpha(opacity=60)";
+    x[0].style.color = "#1d1d1d";
+}
+function meIndigo() {
+    var x = document.getElementsByClassName("me-lepi-kehed");
+    x[0].style.background = "indigo ";
+    x[0].style.opacity = "0.6";
+    x[0].style.filter = "alpha(opacity=60)";
+    x[0].style.color = "white";
+}
+function meDimgray() {
+    var x = document.getElementsByClassName("me-lepi-kehed");
+    x[0].style.background = "dimgray ";
+    x[0].style.opacity = "0.6";
+    x[0].style.filter = "alpha(opacity=60)";
+    x[0].style.color = "white";
+}
+function meCrimson() {
+    var x = document.getElementsByClassName("me-lepi-kehed");
+    x[0].style.background = "crimson ";
+    x[0].style.opacity = "0.6";
+    x[0].style.filter = "alpha(opacity=60)";
+    x[0].style.color = "white";
+}
+
+/* Tampilan Background Apps Tampilan */
+function meOrangeBG() {
+    var x = document.getElementsByClassName("me-lepi-tengah");
+    x[0].style.background = "orange";
+}
+function meCyanBG() {
+    var x = document.getElementsByClassName("me-lepi-tengah");
+    x[0].style.background = "cyan";
+}
+function meIndigoBG() {
+    var x = document.getElementsByClassName("me-lepi-tengah");
+    x[0].style.background = "indigo ";
+}
+function meDimgrayBG() {
+    var x = document.getElementsByClassName("me-lepi-tengah");
+    x[0].style.background = "dimgray ";
+}
+function meCrimsonBG() {
+    var x = document.getElementsByClassName("me-lepi-tengah");
+    x[0].style.background = "crimson ";
+}
+
+/*Reset Apps Tampilan*/
+function meResetAll() {
+    var x = document.getElementsByClassName("me-lepi-tengah");
+    x[0].style.background = "white ";
+    x[0].style.color = "#1d1d1d";
+	var x = document.getElementsByClassName("me-lepi-kehed");
+    x[0].style.background = "orange";
+    x[0].style.opacity = "0.6";
+    x[0].style.filter = "alpha(opacity=60)";
+    x[0].style.color = "#1d1d1d";
+}
